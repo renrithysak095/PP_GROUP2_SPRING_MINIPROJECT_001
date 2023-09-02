c6bf83f2ef1b291ff28e6477a1f5f0dd270a94ed
IamSak <renrithysak095@gmail.com>
2023-09-02 15:18:50 +0700

    add_lombok, merge with developer

--- src/main/java/com/example/mini_project/model/Bookmark.java
+++ src/main/java/com/example/mini_project/model/Bookmark.java
@@ -1,4 +1,11 @@
 package com.example.mini_project.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Bookmark {
 }
